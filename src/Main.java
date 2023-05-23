105191157a363dc53122a57cab8c08786e3a4eaa
LastBeat <devmon0927@gmail.com>
2023-05-24 01:43:46 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 1000; i++) {
             System.out.println("i = " + i);
         }
     }
